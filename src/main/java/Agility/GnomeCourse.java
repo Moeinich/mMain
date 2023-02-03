9acba8cb6345b2a2b23e12c77f705e8c0f013f73
Moeinich <danielbang35@gmail.com>
2023-02-03 13:57:08 +0100

    interaction update

--- src/main/java/Agility/GnomeCourse.java
+++ src/main/java/Agility/GnomeCourse.java
@@ -19,41 +19,41 @@ public class GnomeCourse extends Task {
         GameObject GnomeObstacle1ID = Objects.stream().within(5).id(23145).nearest().first();
         GameObject GnomeObstacle2ID = Objects.stream().within(5).id(23134).nearest().first();
         if (SkillData.GnomeObstacle1Area.contains(Players.local()) && GnomeObstacle1ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Log balance", "Walk-across");
             Condition.wait( () -> GnomeObstacle2ID.inViewport(), 500, 50);
         }
 
         GameObject GnomeObstacle3ID = Objects.stream().within(5).id(23559).nearest().first();
         if (SkillData.GnomeObstacle2Area.contains(Players.local()) && GnomeObstacle2ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Obstacle net", "Climb-over");
             Condition.wait( () -> GnomeObstacle3ID.inViewport(), 500, 50);
         }
 
         GameObject GnomeObstacle4ID = Objects.stream().within(5).id(23557).nearest().first();
         if (SkillData.GnomeObstacle3Area.contains(Players.local()) && GnomeObstacle3ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Tree branch", "Climb");
             Condition.wait( () -> GnomeObstacle4ID.inViewport(), 500, 50);
         }
 
         GameObject GnomeObstacle5ID = Objects.stream().within(5).id(23560).nearest().first();
         if (SkillData.GnomeObstacle4Area.contains(Players.local()) && GnomeObstacle4ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Balacing rope", "Walk-on");
             Condition.wait( () -> GnomeObstacle5ID.inViewport(), 500, 50);
         }
 
         GameObject GnomeObstacle6ID = Objects.stream().within(5).id(23135).nearest().first();
         if (SkillData.GnomeObstacle5Area.contains(Players.local()) && GnomeObstacle5ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Tree branch", "Climb-down");
             Condition.wait( () -> GnomeObstacle6ID.inViewport(), 500, 50);
         }
 
         GameObject GnomeObstacle7ID = Objects.stream().within(5).id(23138).nearest().first();
         if (SkillData.GnomeObstacle6Area.contains(Players.local()) && GnomeObstacle6ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Obstacle net", "Climb-over");
             Condition.wait( () -> GnomeObstacle7ID.inViewport(), 500, 50);
         }
         if (SkillData.GnomeObstacle7Area.contains(Players.local()) && GnomeObstacle7ID.inViewport()) {
-            GnomeObstacle1ID.interact("climb");
+            GnomeObstacle1ID.interact("Obstacle pipe", "Squeeze-through");
             Condition.wait( () -> GnomeObstacle1ID.inViewport(), 500, 50);
             Movement.moveTo(SkillData.GnomeObstacle1Area.getRandomTile());
         }
