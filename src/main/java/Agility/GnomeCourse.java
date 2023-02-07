38f42e2d7d317dd9d6584122585905bef9e36245
Moeinich <danielbang35@gmail.com>
2023-02-07 09:19:04 +0100

    wait till xp gained before clicking next obstacle

--- src/main/java/Agility/GnomeCourse.java
+++ src/main/java/Agility/GnomeCourse.java
@@ -8,12 +8,15 @@ import org.powbot.api.rt4.Objects;
 import org.powbot.api.rt4.Players;
 import org.powbot.api.rt4.Skills;
 import org.powbot.api.rt4.Widgets;
+import org.powbot.api.rt4.walking.model.Skill;
 
 import Helpers.SkillData;
 import Helpers.Task;
 import script.mMain;
 
 public class GnomeCourse extends Task {
+    int CurrentXP = Skills.experience(Skill.Agility);
+
     @Override
     public boolean activate() {
         return Skills.realLevel(Constants.SKILLS_AGILITY) <= 9;
@@ -31,48 +34,57 @@ public class GnomeCourse extends Task {
         GameObject GnomeObstacle1ID = Objects.stream().within(20).id(23145).nearest().first();
         if (SkillData.GnomeObstacle1Area.contains(Players.local()) && GnomeObstacle1ID.inViewport()) {
             mMain.State = "Handle obstacle 1";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle1ID.interact("Walk-across", "Log balance");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
 
         GameObject GnomeObstacle2ID = Objects.stream().within(5).id(23134).nearest().first();
         if (SkillData.GnomeObstacle2Area.contains(Players.local()) && GnomeObstacle2ID.inViewport()) {
             mMain.State = "Handle obstacle 2";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle2ID.interact("Climb-over", "Obstacle net");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
+
         GameObject GnomeObstacle3ID = Objects.stream().within(5).id(23559).nearest().first();
         if (SkillData.GnomeObstacle3Area.contains(Players.local()) && GnomeObstacle3ID.inViewport()) {
             mMain.State = "Handle obstacle 3";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle3ID.interact("Climb", "Tree branch");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
+
         GameObject GnomeObstacle4ID = Objects.stream().within(5).id(23557).nearest().first();
         if (SkillData.GnomeObstacle4Area.contains(Players.local()) && GnomeObstacle4ID.inViewport()) {
             mMain.State = "Handle obstacle 4";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle4ID.interact("Walk-on", "Balancing rope");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
 
         GameObject GnomeObstacle5ID = Objects.stream().within(5).id(23560).nearest().first();
         if (SkillData.GnomeObstacle5Area.contains(Players.local()) && GnomeObstacle5ID.inViewport()) {
             mMain.State = "Handle obstacle 5";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle5ID.interact("Climb-down", "Tree branch");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
 
         GameObject GnomeObstacle6ID = Objects.stream().within(10).id(23135).nearest().first();
         if (SkillData.GnomeObstacle6Area.contains(Players.local()) && GnomeObstacle6ID.inViewport()) {
             mMain.State = "Handle obstacle 6";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle6ID.interact("Climb-over", "Obstacle net");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
 
         GameObject GnomeObstacle7ID = Objects.stream().within(20).id(23138).nearest().first();
         if (SkillData.GnomeObstacle7Area.contains(Players.local()) && GnomeObstacle7ID.inViewport()) {
             mMain.State = "Handle obstacle 7";
+            CurrentXP = Skills.experience(Skill.Agility);
             GnomeObstacle7ID.interact("Squeeze-through", "Obstacle pipe");
-            Condition.wait( () -> (!Players.local().inMotion()), 500, 50);
+            Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility)), 800, 50);
         }
     }
 }
