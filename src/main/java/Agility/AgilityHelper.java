f1ff39281463e097b7f36ca60188c5b90879cfbf
Shadiezz <danielbang35@gmail.com>
2023-02-13 16:51:09 +0100

    Canifis optimization

--- src/main/java/Agility/AgilityHelper.java
+++ src/main/java/Agility/AgilityHelper.java
@@ -15,6 +15,7 @@ import script.mMain;
 
 public class AgilityHelper {
     int CurrentXP = Skills.experience(Skill.Agility);
+    public static String marksName = "Mark of grace";
 
     public void handleObstacle(int obstacleID, String interaction, String interactionName, String state) {
         GameObject obstacleObject = Objects.stream().within(10).id(obstacleID).nearest().first();
@@ -39,6 +40,43 @@ public class AgilityHelper {
             if (obstacleObject.interact(interaction, interactionName)) {
                 Condition.wait( () -> (CurrentXP != Skills.experience(Skill.Agility) || SkillData.CanifisFloorArea.contains(Players.local())), 400, 50);
             }
+        }
+    }
+    enum Canifis {
+        OBS_1(14843, "Climb", "Tall tree", "OBS1"),
+        OBS_2(14844, "Jump", "Gap", "OBS2"),
+        OBS_3(14845, "Jump", "Gap", "OBS3"),
+        OBS_4(14848, "Jump", "Gap", "OBS4"),
+        OBS_5(14846, "Jump", "Gap", "OBS5"),
+        OBS_6(14894, "Vault", "Pole-vault", "OBS&"),
+        OBS_7(14847, "Jump", "Gap", "OBS7"),
+        OBS_8(14897, "Jump", "Gap", "OBS8");
+
+        private final int id;
+        private final String action;
+        private final String name;
+        private final String state;
+
+        Canifis(int id, String action, String name, String state) {
+            this.id = id;
+            this.action = action;
+            this.name = name;
+            this.state = state;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getAction() {
+            return action;
+        }
+        public String getState() {
+            return state;
+        }
+
+        public String getName() {
+            return name;
         }
     }
 }
--- src/main/java/Agility/CanifisCourse_test.java
+++ src/main/java/Agility/CanifisCourse_test.java
@@ -16,44 +16,6 @@ import Helpers.Task;
 import script.mMain;
 
 public class CanifisCourse_test extends Task {
-    String marksName = "Mark of grace";
-    enum Obstacle {
-        OBS_1(14843, "Climb", "Tall tree", "OBS1"),
-        OBS_2(14844, "Jump", "Gap", "OBS2"),
-        OBS_3(14845, "Jump", "Gap", "OBS3"),
-        OBS_4(14848, "Jump", "Gap", "OBS4"),
-        OBS_5(14846, "Jump", "Gap", "OBS5"),
-        OBS_6(14894, "Vault", "Pole-vault", "OBS&"),
-        OBS_7(14847, "Jump", "Gap", "OBS7"),
-        OBS_8(14897, "Jump", "Gap", "OBS8");
-
-        private final int id;
-        private final String action;
-        private final String name;
-        private final String state;
-
-        Obstacle(int id, String action, String name, String state) {
-            this.id = id;
-            this.action = action;
-            this.name = name;
-            this.state = state;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public String getAction() {
-            return action;
-        }
-        public String getState() {
-            return state;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
 
     public boolean activate() {
         return Skills.realLevel(Constants.SKILLS_AGILITY) >= 40 && Skills.realLevel(Constants.SKILLS_AGILITY) <= 80;
@@ -84,13 +46,13 @@ public class CanifisCourse_test extends Task {
 
     public void ShouldRunObstacle() {
         if (SkillData.CanifisFloorArea.contains(Players.local())) {
-            GameObject CanifisObstacle1 = Objects.stream().within(8).id(Obstacle.OBS_1.getId()).nearest().first();
+            GameObject CanifisObstacle1 = Objects.stream().within(8).id(AgilityHelper.Canifis.OBS_1.getId()).nearest().first();
             if (!CanifisObstacle1.inViewport()) {
                 mMain.State = "Move to Canifis start";
                 Movement.moveTo(SkillData.CanifisStart.getRandomTile());
             } else {
                 AgilityHelper helper = new AgilityHelper();
-                helper.handleObstacle(Obstacle.OBS_1.getId(), Obstacle.OBS_1.getAction(), Obstacle.OBS_1.name, "Handle " + Obstacle.OBS_1.getState());
+                helper.handleObstacle(AgilityHelper.Canifis.OBS_1.getId(), AgilityHelper.Canifis.OBS_1.getAction(), AgilityHelper.Canifis.OBS_1.getName(), "Handle " + AgilityHelper.Canifis.OBS_1.getState());
             }
         } else if (SkillData.CanifisObstacle1Bug.contains(Players.local())) {
             mMain.State = "Stuck due to RS bug";
@@ -99,27 +61,27 @@ public class CanifisCourse_test extends Task {
 
         if (SkillData.CanifisObstacle2.contains(Players.local())) {
             AgilityHelper helper = new AgilityHelper();
-            helper.handleObstacleWithLoot(Obstacle.OBS_2.getId(), Obstacle.OBS_2.getAction(), Obstacle.OBS_2.name, "Handle " + Obstacle.OBS_2.getState(), marksName);
+            helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_2.getId(), AgilityHelper.Canifis.OBS_2.getAction(), AgilityHelper.Canifis.OBS_2.getName(), "Handle " + AgilityHelper.Canifis.OBS_2.getState(), AgilityHelper.marksName);
         }
 
         if (SkillData.CanifisObstacle3.contains(Players.local())) {
             AgilityHelper helper = new AgilityHelper();
-            helper.handleObstacleWithLoot(Obstacle.OBS_3.getId(), Obstacle.OBS_3.getAction(), Obstacle.OBS_3.name, "Handle " + Obstacle.OBS_2.getState(), marksName);
+            helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_3.getId(), AgilityHelper.Canifis.OBS_3.getAction(), AgilityHelper.Canifis.OBS_3.getName(), "Handle " + AgilityHelper.Canifis.OBS_2.getState(), AgilityHelper.marksName);
         }
 
         if (SkillData.CanifisObstacle4.contains(Players.local())) {
             AgilityHelper helper = new AgilityHelper();
-            helper.handleObstacleWithLoot(Obstacle.OBS_4.getId(), Obstacle.OBS_4.getAction(), Obstacle.OBS_4.name, "Handle " + Obstacle.OBS_4.getState(), marksName);
+            helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_4.getId(), AgilityHelper.Canifis.OBS_4.getAction(), AgilityHelper.Canifis.OBS_4.getName(), "Handle " + AgilityHelper.Canifis.OBS_4.getState(), AgilityHelper.marksName);
         }
 
         if (SkillData.CanifisObstacle5.contains(Players.local())) {
             AgilityHelper helper = new AgilityHelper();
-            helper.handleObstacleWithLoot(Obstacle.OBS_5.getId(), Obstacle.OBS_5.getAction(), Obstacle.OBS_5.name, "Handle " + Obstacle.OBS_5.getState(), marksName);
+            helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_5.getId(), AgilityHelper.Canifis.OBS_5.getAction(), AgilityHelper.Canifis.OBS_5.getName(), "Handle " + AgilityHelper.Canifis.OBS_5.getState(), AgilityHelper.marksName);
         }
 
         if (SkillData.CanifisObstacle6.contains(Players.local())) {
             AgilityHelper helper = new AgilityHelper();
-            helper.handleObstacleWithLoot(Obstacle.OBS_6.getId(), Obstacle.OBS_6.getAction(), Obstacle.OBS_6.name, "Handle " + Obstacle.OBS_6.getState(), marksName);
+            helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_6.getId(), AgilityHelper.Canifis.OBS_6.getAction(), AgilityHelper.Canifis.OBS_6.getName(), "Handle " + AgilityHelper.Canifis.OBS_6.getState(), AgilityHelper.marksName);
         }
 
         if (SkillData.CanifisObstacle7.contains(Players.local())) {
@@ -129,13 +91,13 @@ public class CanifisCourse_test extends Task {
                 Movement.step(SkillData.CanifisObstacle7MoveTo.getRandomTile());
             } else {
                 AgilityHelper helper = new AgilityHelper();
-                helper.handleObstacleWithLoot(Obstacle.OBS_7.getId(), Obstacle.OBS_7.getAction(), Obstacle.OBS_7.name, "Handle " + Obstacle.OBS_6.getState(), marksName);
+                helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_7.getId(), AgilityHelper.Canifis.OBS_7.getAction(), AgilityHelper.Canifis.OBS_7.getName(), "Handle " + AgilityHelper.Canifis.OBS_6.getState(), AgilityHelper.marksName);
             }
         }
 
         if (SkillData.CanifisObstacle8.contains(Players.local())) {
             AgilityHelper helper = new AgilityHelper();
-            helper.handleObstacleWithLoot(Obstacle.OBS_8.getId(), Obstacle.OBS_8.getAction(), Obstacle.OBS_8.name, "Handle " + Obstacle.OBS_8.getState(), marksName);
+            helper.handleObstacleWithLoot(AgilityHelper.Canifis.OBS_8.getId(), AgilityHelper.Canifis.OBS_8.getAction(), AgilityHelper.Canifis.OBS_8.getName(), "Handle " + AgilityHelper.Canifis.OBS_8.getState(), AgilityHelper.marksName);
         }
     }
 }
