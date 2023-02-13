47c7352bea8e80d3a0f7c1ac060a9ed391e25d75
Moeinich <danielbang35@gmail.com>
2023-02-13 11:10:17 +0100

    World hop works now?

--- src/main/java/Thieving/FruitStall.java
+++ src/main/java/Thieving/FruitStall.java
@@ -128,7 +128,7 @@ public class FruitStall extends Task {
         mMain.State = "Worldhopping";
         int[] p2p = SkillData.p2p;
         int randomWorld = p2p[Random.nextInt(0, p2p.length - 1)];
-        World world = new World(2, randomWorld, 1, World.Type.MEMBERS, World.Server.RUNE_SCAPE, World.Specialty.NONE);
+        World world = new World(randomWorld, randomWorld, 1, World.Type.MEMBERS, World.Server.RUNE_SCAPE, World.Specialty.NONE);
         world.hop();
     }
     private void WalkToSpot() {
