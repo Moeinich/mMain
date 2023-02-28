6365213b3f84e39c26e0f068f42db62ad89bad95
Shadiezz <danielbang35@gmail.com>
2023-02-28 19:31:02 +0100

    refactor to kotlin

---
+++ src/main/java/helpers/extentions/Conditions.kt
@@ -0,0 +1,95 @@
+package helpers.extentions
+
+import org.powbot.api.rt4.*
+import org.powbot.api.rt4.Skills.experience
+import org.powbot.api.rt4.walking.model.Skill
+import java.util.concurrent.Callable
+
+object Conditions {
+    fun waitUntilItemEntersInventory(
+        itemName: String,
+        itemCount: Int,
+    ): () -> Boolean {
+        return {
+            val newCount: Int = Inventory.stream().name(itemName)
+                .toList().sumOf { obj: Item -> obj.stack }
+            newCount > itemCount
+        }
+    }
+
+    fun waitUntilItemEntersInventory(
+        itemId: Int,
+        itemCount: Int,
+    ): () -> Boolean {
+        return {
+            val newCount: Int = Inventory.stream().id(itemId)
+                .toList().sumOf { obj: Item -> obj.stack }
+            newCount > itemCount
+        }
+    }
+
+    fun waitUntilItemLeavesInventory(
+        itemName: String,
+        itemCount: Int,
+    ): () -> Boolean {
+        return {
+            val newCount: Int = Inventory.stream().name(itemName)
+                .toList().sumOf { obj: Item -> obj.stack }
+            newCount < itemCount
+        }
+    }
+
+    fun waitUntilAnyItemEntersInventory(): Callable<Boolean> {
+        val originalCount = Inventory.emptySlotCount()
+        return Callable<Boolean> {
+            originalCount > Inventory.emptySlotCount()
+        }
+    }
+
+    fun waitUntilNotMoving(): Callable<Boolean> {
+        return Callable<Boolean> { !Players.local().inMotion() }
+    }
+
+    fun waitUntilChatting(): Callable<Boolean> {
+        return Callable<Boolean> { Chat.chatting() }
+    }
+
+    fun waitUntilNotChatting(): Callable<Boolean> {
+        return Callable<Boolean> { !Chat.chatting() }
+    }
+
+    fun waitUntilNonIdleAnimation(): Callable<Boolean> {
+        return Callable<Boolean> { Players.local().animation() != -1 }
+    }
+
+    fun waitUntilIdleAnimation(): Callable<Boolean> {
+        return Callable<Boolean> { Players.local().animation() == -1 }
+    }
+
+    @JvmStatic
+    fun expGained(skill: Skill?): Boolean {
+        val startExp = experience(skill!!)
+        return experience(skill) > startExp
+    }
+
+    fun waitUntilComponentAppears(widget: Int, component: Int): Callable<Boolean> {
+        return Callable<Boolean> { Widgets.component(widget, component).visible() }
+    }
+
+    fun waitUntilComponentAppears(widget: Int, component: Int, subComponent: Int): Callable<Boolean> {
+        return Callable<Boolean> { Widgets.component(widget, component, subComponent).visible() }
+    }
+
+    fun waitUntilNpcAppears(id: Int): Callable<Boolean> {
+        return Callable<Boolean> { Npcs.stream().id(id).first() != Npc.Nil }
+    }
+
+    fun waitUntilNpcAppears(name: String): Callable<Boolean> {
+        return Callable<Boolean> { Npcs.stream().name(name).first() != Npc.Nil }
+    }
+
+    fun healthDecreases(): Callable<Boolean> {
+        val currentHp = Combat.health()
+        return Callable { Combat.health() < currentHp }
+    }
+}
